32434f7ac67f70a902c8f5743679d0538b43ac16
SNijanthan <nijanthan378@gmail.com>
2025-03-13 21:22:07 +0530

    config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Feed from "./components/Feed";
 
 const App = () => {
   return (
     <>
-      <h1 className="">Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/feed" element={<Feed />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
